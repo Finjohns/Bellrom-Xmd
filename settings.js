47c454b9acfce032ba81c941578e7d4a35e5fd98
Finjohns <kelvinonginjo08@gmail.com>
2025-02-28 18:56:58 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,42 +5,51 @@
 
 const fs = require('fs')
 const chalk = require('chalk')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
+
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
 //owmner v card
 global.ytname = "YT: JINWIILONGINJO" //ur yt chanel name
-global.socialm = "IG: JINWIIL ONGINJO" //ur github or insta name
+global.socialm = "IG: @JINWIILONGINJO" //ur github or insta name
 global.location = "Kenya" //ur location
 
 //new
-global.botname = '𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //ur bot name
-global.ownernumber = '254769365617' //ur owner number
-global.ownername = '© JINWIILONGINJO' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaxZbeSDTkJwBgUb9u3N" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/EqDVC9BGF9k86ZMHdjxY0d" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.botname = process.env.BOT_NAME ||'𝐁𝐞 𝐗𝐦𝐝' //enter your  bot name here
+global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
+global.ownername = '© TENNOMODZ' //ur owner name
+global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.wagc = "https://chat.whatsapp.com/CzFlFQrkdzxFw0pxCBYM7H" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
 global.themeemoji = '🪀'
-global.wm = "JINWIIL ONGINJO"
-global.botscript = 'https://whatsapp.com/channel/0029VaxZbeSDTkJwBgUb9u3N' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
-global.packname = "VINKEL"
-global.author = "JINWIIL"
-global.creator = "254769365617@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["254769365617"] // Premium User
+global.wm = "GIDDY TENNOR"
+global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
+global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers author name here
+global.author = "Giddy-Tennor"
+global.creator = "254703726139@s.whatsapp.net"
+global.xprefix = process.env.BOT_PREFIX ||'.'
+global.premium = ["254703726139"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v2' // menu type 'v1' => 'v8'
-global.typereply = 'v1' // reply type 'v1' => 'v3'
-global.autoblocknumber = '263,234' //set autoblock country code
-global.antiforeignnumber = '' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = true //auto status/story view
+global.typemenu = process.env.TYPE_MENU || 'v2' // menu type 'v1' => 'v8' 
+global.typereply = process.env.TYPE_REPLY ||'v1' // reply type 'v1' => 'v3'
+global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
+global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
+global.welcome = process.env.WELCOME || 'false' //welcome/left in groups
+global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
+global.autoswview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
 global.adminevent = true //show promote/demote message
 global.groupevent = true //show update messages in group chat
 //msg
+global.autorecord = process.env.AUTO_RECORD || 'false'
+global.autorecordtype = process.env.AUTO_RECORDTYPE || 'false'
+
 global.mess = {
 	limit: 'Your limit is up <\>',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	owner: 'Bellah Xmd owner only<\>',
+    admin: 'Bot is not admin<\>',
+    group: 'feature for group only<\>',
     done: 'Done ✓',
     error: 'Error !',
     success: 'Succes •'
@@ -55,3 +64,4 @@ fs.watchFile(file, () => {
     delete require.cache[file]
     require(file)
 })
+	
